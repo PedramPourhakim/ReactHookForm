c69db27c8f703b7f2bee296827ca00c6f086d324
Pedram <shemuel1226@gmail.com>
2024-07-26 22:09:49 +0330

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {z} from 'zod';
+import { SubmitHandler, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod"
+
+const schema = z.object({
+  email: z.string().email(),
+  password: z.string().min(8)
+})
+
+type FormFields = z.infer<typeof schema>;
+
+
 
 function App() {
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors, isSubmitting },
+  } = useForm<FormFields>({
+    defaultValues:{
+      email : "test@email.com",
+      password: "12345678"
+    },
+    resolver : zodResolver(schema)
+  });
+
+  const onSubmit: SubmitHandler<FormFields> = async (data) => {
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      throw new Error();
+      console.log(data);
+    } catch (error) {
+      // setError("email",{
+      //   message: "This email is already taken",
+      // });
+      setError("root",{
+        message: "This email is already taken",
+      });
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form className="App" onSubmit={handleSubmit(onSubmit)}>
+      <input
+        {...register("email")}
+        type="text"
+        placeholder="Email"
+      />
+      {errors.email && <div>{errors.email.message}</div>}
+      <input
+        {...register("password")}
+        type="password"
+        placeholder="Password"
+      />
+      {errors.password && <div>{errors.password.message}</div>}
+      <button disabled={isSubmitting} type="submit">
+        {isSubmitting ? "Loading...." : "Submit"}
+      </button>
+      {errors.root && <div>{errors.root.message}</div>}
+    </form>
   );
 }
 
